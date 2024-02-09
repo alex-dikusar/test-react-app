35263b64b59e4b307c3699f5ca74c67989e2e740
Alexander Dikusar <dikusaralexander@gmail.com>
2024-02-09 21:16:43 +0300

    update packages

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -4,5 +4,5 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
 
-  expect(screen.getByText('AAAAAAAAAAAAAA')).toBeInTheDocument();
+  expect(screen.getByText('App Layout')).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import AppLayout from 'src/components/AppLayout';
 
 function App() {
-  // eslint-disable-next-line no-console
-  console.log(import.meta.env);
-
-  return <AppLayout>AAAAAAAAAAAAAA</AppLayout>;
+  return <AppLayout>App Layout</AppLayout>;
 }
 
 export default App;
